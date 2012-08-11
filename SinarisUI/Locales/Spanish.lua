b3d32ad45c81b40bb2c8f6bd954d93db88d88954
Natias Rupp <natiasrupp@web.de>
2012-08-11 15:46:14 +0200

    update

---
+++ SinarisUI/Locales/Spanish.lua
@@ -0,0 +1,11 @@
+---------------------------------------------------------------------------------------------
+-- Project: SinarisUI
+---------------------------------------------------------------------------------------------
+
+local S, C, L, G = unpack( Tukui )
+
+if( S.client ~= "esES" or S.client ~= "esMX" ) then return end
+
+if( TukuiConfigUILocalization ) then
+
+end
